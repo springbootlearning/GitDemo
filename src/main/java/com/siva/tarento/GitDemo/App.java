5cd1326c57ff1510f941a148c1ea5c59d48b2a15
springbootlearning <41922166+springbootlearning@users.noreply.github.com>
2018-07-31 15:55:27 +0530

    Add files via upload
    sample git testing

---
+++ src/main/java/com/siva/tarento/GitDemo/App.java
@@ -0,0 +1,21 @@
+package com.siva.tarento.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData(String data)
+	{
+		
+		if("Siva".equalsIgnoreCase("Siva"))
+		System.out.println("dataa"+data);
+		
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
